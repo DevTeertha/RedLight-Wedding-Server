56d24925788bed3f8de4cb9a1ca87404c6fbc23b
DevTeertha <devteertha28@gmail.com>
2022-04-28 04:56:50 +0600

    make it better

--- index.js
+++ index.js
@@ -1,148 +1,148 @@
-const express = require('express')
-var compression = require('compression');
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const fileUpload = require('express-fileupload');
-const MongoClient = require('mongodb').MongoClient;
-const ObjectID = require('mongodb').ObjectID;
+const express = require("express");
+var compression = require("compression");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const fileUpload = require("express-fileupload");
+const MongoClient = require("mongodb").MongoClient;
+const ObjectID = require("mongodb").ObjectID;
 
-const app = express()
-require('dotenv').config();
+const app = express();
+require("dotenv").config();
 
-app.use(cors())
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(fileUpload());
 app.use(compression());
 
-
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gqnhb.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-client.connect(err => {
+client.connect((err) => {
   const redLightAdmin = client.db(process.env.DB_NAME).collection("admin");
-  const redLightBookings = client.db(process.env.DB_NAME).collection("bookings");
-  const redLightServices = client.db(process.env.DB_NAME).collection("services");
+  const redLightBookings = client
+    .db(process.env.DB_NAME)
+    .collection("bookings");
+  const redLightServices = client
+    .db(process.env.DB_NAME)
+    .collection("services");
   const redLightReviews = client.db(process.env.DB_NAME).collection("reviews");
 
-  app.get('/isAdmin', (req, res) => {
+  app.get("/isAdmin", (req, res) => {
     const isAdminEmail = req.query.email;
-    redLightAdmin.findOne({ email: isAdminEmail })
-      .then(response => {
-        if (response.email.length > 0) {
-          res.send(true)
-        }
-        else {
-          res.send(false)
-        }
-      })
-  })
+    redLightAdmin.findOne({ email: isAdminEmail }).then((response) => {
+      if (response.email.length > 0) {
+        res.send(true);
+      } else {
+        res.send(false);
+      }
+    });
+  });
 
-  app.get('/reviews', (req, res) => {
-    redLightReviews.find({})
-      .toArray((err, doc) => {
-        res.send(doc)
-      })
-  })
+  app.get("/reviews", (req, res) => {
+    redLightReviews.find({}).toArray((err, doc) => {
+      res.send(doc);
+    });
+  });
 
-  app.get('/services', (req, res) => {
-    redLightServices.find({})
-      .toArray((err, document) => {
-        console.log('Data Collected');
-        res.send(document)
-      })
-  })
+  app.get("/services", (req, res) => {
+    redLightServices.find({}).toArray((err, document) => {
+      console.log("Data Collected");
+      res.send(document);
+    });
+  });
 
-  app.get('/bookings', (req, res) => {
-    redLightBookings.find({})
-      .toArray((err, document) => {
-        res.send(document)
-      })
-  })
+  app.get("/bookings", (req, res) => {
+    redLightBookings.find({}).toArray((err, document) => {
+      res.send(document);
+    });
+  });
 
-  app.get('/userBookingList', (req, res) => {
+  app.get("/userBookingList", (req, res) => {
     const userEmail = req.query.email;
-    redLightBookings.find({ email: userEmail })
-      .toArray((err, doc) => {
-        res.send(doc);
-      })
-  })
+    redLightBookings.find({ email: userEmail }).toArray((err, doc) => {
+      res.send(doc);
+    });
+  });
 
-  app.post('/makeAdmin', (req, res) => {
+  app.post("/makeAdmin", (req, res) => {
     const email = req.body.email;
 
-    redLightAdmin.insertOne({ email: email })
-      .then(response => {
+    redLightAdmin.insertOne({ email: email }).then((response) => {
+      res.send(response.insertedCount > 0);
+    });
+  });
+
+  app.post("/bookNow", (req, res) => {
+    const bookingInfo = req.body;
+    redLightBookings
+      .insertOne(bookingInfo)
+      .then((response) => {
         res.send(response.insertedCount > 0);
       })
-  })
+      .catch((err) => res.send(err));
+  });
 
-
-  app.post('/bookNow', (req, res) => {
-    const bookingInfo = req.body;
-    redLightBookings.insertOne(bookingInfo)
-      .then(response => {
-        res.send(response.insertedCount > 0)
-      })
-      .catch(err => res.send(err))
-  })
-
-  app.post('/addService', (req, res) => {
+  app.post("/addService", (req, res) => {
     const file = req.files.file;
     const title = req.body.title;
     const price = req.body.price;
     const description = req.body.description;
 
     const newImg = file.data;
-    const encImg = newImg.toString('base64');
+    const encImg = newImg.toString("base64");
 
     const image = {
       contentType: file.mimetype,
       size: file.size,
-      img: Buffer.from(encImg, 'base64')
-    }
+      img: Buffer.from(encImg, "base64"),
+    };
 
-    redLightServices.insertOne({title , price , description , img:image })
-    .then(response=>{
-      console.log(response.insertedCount>0)
-      req.send(response.insertedCount>0)
-    })
-    .catch(err=>res.send(false))
-  })
+    redLightServices
+      .insertOne({ title, price, description, img: image })
+      .then((response) => {
+        console.log(response.insertedCount > 0);
+        req.send(response.insertedCount > 0);
+      })
+      .catch((err) => res.send(false));
+  });
 
-  app.post('/addReview', (req, res) => {
+  app.post("/addReview", (req, res) => {
     const reviewInfo = req.body;
-    redLightReviews.insertOne(reviewInfo)
-      .then(response => {
-        res.send(response.insertedCount > 0)
-      })
-  })
+    redLightReviews.insertOne(reviewInfo).then((response) => {
+      res.send(response.insertedCount > 0);
+    });
+  });
 
-  app.put('/updateStatus', (req, res) => {
+  app.put("/updateStatus", (req, res) => {
     const data = req.body;
-    redLightBookings.updateOne({ _id: new ObjectID(data.id) }, {
-      $set: { "status": data.status },
-      $currentDate: { "lastModified": true }
-    })
-      .then(response => {
+    redLightBookings
+      .updateOne(
+        { _id: new ObjectID(data.id) },
+        {
+          $set: { status: data.status },
+          $currentDate: { lastModified: true },
+        }
+      )
+      .then((response) => {
         res.send(response.modifiedCount > 0);
-      })
-  })
+      });
+  });
 
-  app.delete('/deleteService/:id', (req, res) => {
+  app.delete("/deleteService/:id", (req, res) => {
     const deleteID = req.params.id;
-    redLightServices.deleteOne({ _id: new ObjectID(deleteID) })
-      .then(response => res.send(response.deletedCount > 0))
-      .catch(err => res.send(err))
-  })
+    redLightServices
+      .deleteOne({ _id: new ObjectID(deleteID) })
+      .then((response) => res.send(response.deletedCount > 0))
+      .catch((err) => res.send(err));
+  });
 });
 
+app.get("/", (req, res) => {
+  res.send("RedLight Weeding Server Running!");
+});
 
-
-app.get('/', (req, res) => {
-  res.send('RedLight Weeding Server Running!');
-})
-
-app.listen(5000)
+app.listen(5000 || 8080);
